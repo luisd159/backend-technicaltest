febc768a5cb06582899ea6c98cc0fd4968271cad
Luis E Avila <avilale@uninorte.edu.co>
2024-01-25 22:29:57 -0500

    add .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NoteModule } from './note/note.module';
 import { Note } from './note/entity/note.entity';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const db_option = {
-  host: process.env.HOST,
-  port: parseInt(process.env.DB_PORT),
-  password: process.env.DB_PASSWORD,
-  username: process.env.DB_USERNAME,
-  database: process.env.DB_NAME,
-};
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      ...db_option,
+      host: process.env.HOST,
+      port: parseInt(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
       entities: [Note],
       synchronize: true,
       logging: true,
